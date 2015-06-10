dc9ccdf13432ee5b237dcf58b22f5839703e77f3
unknown <Default@R9MAR4V.insags.com>
2015-06-10 21:41:39 +0200

    Incluidas validaciones

--- ejercicio3/public/producto-angularjs/js/controller/ProductoCtrl.js
+++ ejercicio3/public/producto-angularjs/js/controller/ProductoCtrl.js
@@ -21,31 +21,24 @@ app.controller('ProductoCtrl', [ 'ProductoService', function(productoService) {
 	}
 
 	this.guardar = function() {
-		// TODO Mejorar validaciones de precio
-		if (/[a-z0-9 ]{2,}/i.test(this.model.nombre) && 
-				/[0-9]+/.test(this.model.precio)) {
-				
-			var p = {
-				nombre: this.model.nombre,
-				precio: this.model.precio
-			};		
+		var p = {
+			nombre: this.model.nombre,
+			precio: this.model.precio
+		};		
 
-			if (this.model.id) {
-				p.id = this.model.id;
-				productoService.modificar(p);	
-			} else {
-				productoService.agregar(p);	
-			}
-
-			this.model = {
-				id: null,
-				nombre: null,
-				precio: null
-			};
-			//imprimirProductos(); // automático
+		if (this.model.id) {
+			p.id = this.model.id;
+			productoService.modificar(p);	
 		} else {
-				window.alert("Nombre o precio inválido");
+			productoService.agregar(p);	
 		}
+
+		this.model = {
+			id: null,
+			nombre: null,
+			precio: null
+		};
+		//imprimirProductos(); // automático
 	};
 
 	var initProductoUtil = function() {
