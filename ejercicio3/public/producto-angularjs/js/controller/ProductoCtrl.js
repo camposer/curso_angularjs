b3e5b496bba7ad2bbd941245f59f1c79da0f5483
unknown <Default@R9MAR4V.insags.com>
2015-06-10 20:58:35 +0200

    Solución del ejercicio3 (parte2 - faltan validaciones y directivas)

--- ejercicio3/public/producto-angularjs/js/controller/ProductoCtrl.js
+++ ejercicio3/public/producto-angularjs/js/controller/ProductoCtrl.js
@@ -1,6 +1,53 @@
 app.controller('ProductoCtrl', [ 'ProductoService', function(productoService) {
 	this.productos = productoService.obtenerTodos;
 
+	this.model = {
+		id: null,
+		nombre: null,
+		precio: null
+	};
+
+	this.eliminar = function(id) {
+		productoService.eliminar(id);
+	};
+
+	this.mostrar = function(id) {
+		var p = productoService.obtener(id);
+		this.model = {
+			id: p.id,
+			nombre: p.nombre,
+			precio: p.precio
+		};
+	}
+
+	this.guardar = function() {
+		// TODO Mejorar validaciones de precio
+		if (/[a-z0-9 ]{2,}/i.test(this.model.nombre) && 
+				/[0-9]+/.test(this.model.precio)) {
+				
+			var p = {
+				nombre: this.model.nombre,
+				precio: this.model.precio
+			};		
+
+			if (this.model.id) {
+				p.id = this.model.id;
+				productoService.modificar(p);	
+			} else {
+				productoService.agregar(p);	
+			}
+
+			this.model = {
+				id: null,
+				nombre: null,
+				precio: null
+			};
+			//imprimirProductos(); // automático
+		} else {
+				window.alert("Nombre o precio inválido");
+		}
+	};
+
 	var initProductoUtil = function() {
 		productoService.agregar({
 			nombre: 'TV',
